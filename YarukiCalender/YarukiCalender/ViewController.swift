11bdceffcd23bf616ea54b326603037145f99152
I.D <daisuke.workin@gmail.com>
2020-12-18 00:47:58 +0900

    initialize

---
+++ YarukiCalender/YarukiCalender/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  YarukiCalender
+//
+//  Created by 石澤大輔 on 2020/12/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
