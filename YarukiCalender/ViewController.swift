4b2f10031e0876579e1ce8f2e8aa83d05d81cbbf
I.D <daisuke.workin@gmail.com>
2021-01-30 18:44:54 +0900

    sectionにlayoutを加えられた

--- YarukiCalender/ViewController.swift
+++ YarukiCalender/ViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate{
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout{
 
     let date = Date()
     @IBOutlet private weak var collection: UICollectionView!
@@ -21,6 +21,10 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
 
         collection.dataSource = self
         collection.delegate = self
+
+        let layout = UICollectionViewFlowLayout()
+        layout.sectionInset = UIEdgeInsets(top: 15, left: 15, bottom: 15, right: 15)
+        collection.collectionViewLayout = layout
     }
 
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
@@ -32,5 +36,11 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
         print(indexPath.item)
         cell.backgroundColor = UIColor.red
         return cell
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let horizonSpace: CGFloat = 20
+        let cellSize: CGFloat = 20
+        return CGSize(width: horizonSpace, height: cellSize)
     }
 }
